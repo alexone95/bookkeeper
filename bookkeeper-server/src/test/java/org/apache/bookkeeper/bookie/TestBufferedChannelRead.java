a953463acf6f60590708513c760d11e8a29d7282
alexone95 <alessandropontis95@gmail.com>
2022-07-04 14:43:20 +0200

    Added new tests

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/TestBufferedChannelRead.java
@@ -0,0 +1,98 @@
+package org.apache.bookkeeper.bookie;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+import java.util.Collection;
+import static org.apache.bookkeeper.bookie.TestBufferedChannelWrite.generateByteBuf;
+
+@RunWith(Parameterized.class)
+public class TestBufferedChannelRead {
+
+    private final int bufferedChannelCapacity;
+    private final ByteBuf dstBuffer;
+    private final long bufferedChannelPos;
+    private final int bufferedChannelLength;
+    private final boolean resetIndex;
+    private final int writeBufferStartPosition;
+    private final Object testResult;
+
+    private BufferedChannel bufferedChannel;
+    private static FileChannel bufferedChannelFileChannel;
+
+    @Parameterized.Parameters
+    public static Collection BufferedChannelParameters() {
+        return Arrays.asList(new Object[][] {
+
+                // Suite minimale
+                {1, null, -1, 0, true,0,0},
+                {1, generateByteBuf(1024, false), 0, 1, false, 0,1},
+                {1, generateByteBuf(1024, false), 2, 2, false, 0,2},
+
+        });
+    }
+
+    public TestBufferedChannelRead(int capacity, ByteBuf dst, long pos, int length, boolean resetIndex,
+                                   int writeBufferStartPosition, Object result){
+        this.dstBuffer = dst;
+        this.bufferedChannelPos = pos;
+        this.bufferedChannelLength = length;
+        this.bufferedChannelCapacity = capacity;
+        this.resetIndex = resetIndex;
+        this.writeBufferStartPosition = writeBufferStartPosition;
+        this.testResult = result;
+    }
+
+    @Before
+    public void configureTest() throws IOException {
+
+        ByteBufAllocator allocator = UnpooledByteBufAllocator.DEFAULT;
+        ByteBuf source;
+        File newLogFile = File.createTempFile("test", "log");
+        newLogFile.deleteOnExit();
+        bufferedChannelFileChannel = new RandomAccessFile(newLogFile, "rw").getChannel();
+
+        if (resetIndex) {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(writeBufferStartPosition);
+            bufferedChannelFileChannel.write(byteBuffer);
+            source = generateByteBuf(8, resetIndex);
+        }
+        else
+            source = generateByteBuf(8, resetIndex);
+
+        int bufferedChannelUnpersistedBytesBound = 10;
+        bufferedChannel = new BufferedChannel(allocator, bufferedChannelFileChannel,
+                bufferedChannelCapacity, bufferedChannelUnpersistedBytesBound);
+
+        bufferedChannel.write(source);
+    }
+
+    @After
+    public void close() throws IOException {
+        bufferedChannelFileChannel.close();
+    }
+
+    @Test
+    public void testRead() {
+        try {
+            // Assert that the number of readed bytes is equal to the expected value
+            Assert.assertEquals(testResult, bufferedChannel.read(dstBuffer, bufferedChannelPos, bufferedChannelLength));
+        } catch (Exception e) {
+            Assert.assertEquals(testResult, e.getClass());
+        }
+    }
+
+}
---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/TestBufferedChannelWrite.java
@@ -0,0 +1,93 @@
+package org.apache.bookkeeper.bookie;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
+
+import io.netty.buffer.UnpooledByteBufAllocator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+@RunWith(Parameterized.class)
+public class TestBufferedChannelWrite {
+
+    private final ByteBuf source;
+    private final long byteCapacity;
+    private final Object testResult;
+
+    private BufferedChannel bufferedChannel;
+    private final static long HEADER_SIZE = 32L;
+
+    @Parameterized.Parameters
+    public static Collection BufferedChannelParameters() {
+        return Arrays.asList(new Object[][] {
+                // Suite di test
+                {null, 0, new NullPointerException()},
+                {generateByteBuf(0, false), 1, 0L},
+                {generateByteBuf(1, false), 1, 1L + HEADER_SIZE},
+        });
+
+    }
+
+    public TestBufferedChannelWrite(ByteBuf byteBuf, int byteCapacity, Object testResult){
+        this.source = byteBuf;
+        this.byteCapacity = byteCapacity + HEADER_SIZE;
+        this.testResult = testResult;
+    }
+
+    public static ByteBuf generateByteBuf(int length, boolean resetIndex) {
+        Random random = new Random();
+        byte[] data = new byte[length];
+        ByteBuf byteBuffer;
+        try {
+            random.nextBytes(data);
+            byteBuffer = Unpooled.buffer(1024);
+        }
+        catch (IllegalArgumentException e){
+            return null;
+        }
+        byteBuffer.writeLong(0);
+        byteBuffer.writeLong(1);
+        byteBuffer.writeLong(2);
+        byteBuffer.writeLong(length);
+        byteBuffer.writeBytes(data);
+        if (resetIndex){
+            byteBuffer.resetWriterIndex();
+        }
+        return byteBuffer;
+    }
+
+    @Test
+    public void testWrite() throws Exception {
+        try {
+            bufferedChannel.write(source);
+            // Assert that the size of the filechannel is equal to the expected value
+            Assert.assertEquals((long) testResult, bufferedChannel.fileChannel.size());
+        } catch (Exception e){
+            Assert.assertEquals(testResult.toString(), e.toString());
+        }
+    }
+
+    @Before
+    public void initializeBufferedChannel() throws IOException {
+        File tempLogFile = File.createTempFile("test", "log");
+        tempLogFile.deleteOnExit();
+        FileChannel fileChannel = new RandomAccessFile(tempLogFile, "rw").getChannel();
+        bufferedChannel = new BufferedChannel(UnpooledByteBufAllocator.DEFAULT, fileChannel, (int) byteCapacity);
+    }
+
+    @After
+    public void close() throws IOException {
+        bufferedChannel.close();
+    }
+}
